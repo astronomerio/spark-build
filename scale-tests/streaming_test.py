546a3b2e8c21ee8f85f0d61731df944aba84e266
Sam Tran <stran@mesosphere.com>
2018-05-02 14:41:57 -0700

    Cleanup script

--- scale-tests/streaming_test.py
+++ scale-tests/streaming_test.py
@@ -2,65 +2,71 @@ import json
 import sys
 
 import sdk_cmd
-import shakedown
 import spark_utils as utils
-from tests import test_kafka
 
 KAFKA_PACKAGE_NAME = "kafka"
 KAFKA_SERVICE_NAME = "kafka"
 PRODUCER_SERVICE_NAME = "Spark->Kafka Producer"
+KERBEROS_FLAG = "false"
+JAR_URI = "https://s3-us-west-2.amazonaws.com/infinity-artifacts/soak/spark/dcos-spark-scala-tests-assembly-0.1-SNAPSHOT.jar"
 
 
-def run_pipeline(kerberos_flag, num_consumers, stop_count, jar_uri, spark_app_name):
-    stop_count = str(stop_count)
+def run_pipeline(stop_count, spark_app_name):
     broker_dns = _kafka_broker_dns()
     topic = "top1"
 
-    big_file, big_file_url = "file:///mnt/mesos/sandbox/big.txt", "http://norvig.com/big.txt"
-
     # arguments to the application
-    producer_args = " ".join([broker_dns, big_file, topic, kerberos_flag])
-
-    uris = "spark.mesos.uris=http://norvig.com/big.txt"
-
     common_args = [
-        "--conf", "spark.mesos.containerizer=mesos",
         "--conf", "spark.scheduler.maxRegisteredResourcesWaitingTime=2400s",
         "--conf", "spark.scheduler.minRegisteredResourcesRatio=1.0",
-        "--conf", uris
+        "--conf", "spark.mesos.containerizer=mesos",
+        "--conf", "spark.mesos.executor.docker.image=mesosphere/spark-dev:931ca56273af913d103718376e2fbc04be7cbde0",
+        "--conf", "spark.mesos.uris=http://norvig.com/big.txt"
     ]
 
-    producer_config = ["--conf", "spark.cores.max=2", "--conf", "spark.executor.cores=2",
-                       "--class", "KafkaFeeder"] + common_args
+    producer_id = submit_producer(broker_dns, common_args, topic, spark_app_name)
 
-    producer_id = utils.submit_job(app_url=jar_uri,
+    submit_consumers(broker_dns, common_args, topic, spark_app_name, str(stop_count))
+
+
+def submit_producer(broker_dns, args, topic, spark_app_name):
+    big_file = "file:///mnt/mesos/sandbox/big.txt"
+
+    producer_args = " ".join([broker_dns, big_file, topic, KERBEROS_FLAG])
+
+    print("producer_args: {}".format(producer_args))
+
+    producer_config = ["--conf", "spark.cores.max=2",
+                       "--conf", "spark.executor.cores=2",
+                       "--class", "KafkaFeeder"] + args
+
+    producer_id = utils.submit_job(app_url=JAR_URI,
                                    app_args=producer_args,
                                    app_name=spark_app_name,
                                    args=producer_config,
                                    verbose=False)
+    return producer_id
 
-    shakedown.wait_for(lambda: test_kafka._producer_launched(), ignore_exceptions=False, timeout_seconds=600)
-    # shakedown.wait_for(lambda: utils.is_service_ready(KAFKA_SERVICE_NAME, 1),
-    #                   ignore_exceptions=False, timeout_seconds=600)
 
-    consumer_config = ["--conf", "spark.cores.max=4", "--class", "KafkaConsumer"] + common_args
+def submit_consumers(broker_dns, args, topic, spark_app_name, stop_count):
+    consumer_args = " ".join([broker_dns, topic, stop_count, KERBEROS_FLAG])
 
-    consumer_args = " ".join([broker_dns, topic, stop_count, kerberos_flag])
+    consumer_config = ["--conf", "spark.cores.max=4",
+                       "--class", "KafkaConsumer"] + args
 
-    for i in range(0, num_consumers):
-        utils.run_tests(app_url=jar_uri,
-                        app_args=consumer_args,
-                        expected_output="Read {} words".format(stop_count),
-                        app_name=spark_app_name,
-                        args=consumer_config)
-
-    utils.kill_driver(producer_id, spark_app_name)
+    for i in range(0, 5):
+        utils.submit_job(app_url=JAR_URI,
+                         app_args=consumer_args,
+                         app_name=spark_app_name,
+                         args=consumer_config,
+                         verbose=False)
 
 
 def _kafka_broker_dns():
     cmd = "{package_name} --name={service_name} endpoints broker".format(
-        package_name=KAFKA_PACKAGE_NAME, service_name=KAFKA_SERVICE_NAME)
-    rt, stdout, stderr = sdk_cmd.run_raw_cli(cmd)
+        package_name=KAFKA_PACKAGE_NAME,
+        service_name=KAFKA_SERVICE_NAME)
+    rt, stdout, _ = sdk_cmd.run_raw_cli(cmd)
     assert rt == 0, "Failed to get broker endpoints"
     return json.loads(stdout)["dns"][0]
 
@@ -68,33 +74,27 @@ def _kafka_broker_dns():
 if __name__ == "__main__":
     usage = """
         Setup: export PYTHONPATH=../spark-testing:../testing:../tests
-        Usage: python streaming_test.py number_of_consumers [stop_count]
+        Usage: python streaming_test.py [dispatcher_file] 
     """
 
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         print(usage)
         sys.exit(2)
 
-    jar_uri = "https://s3-us-west-2.amazonaws.com/infinity-artifacts/soak/spark/dcos-spark-scala-tests-assembly-0.1-SNAPSHOT.jar"
+    dispatchers_file = sys.argv[1]
+    print("dispatchers_file: {}".format(dispatchers_file))
+    with open(dispatchers_file) as f:
+        dispatchers = f.read().splitlines()
+    print("dispatchers: {}".format(dispatchers))
 
-    num_consumers = sys.argv[1]
-    print("num_consumers: {}".format(num_consumers))
+    print("Installing kafka CLI...")
+    rc, stdout, stderr = sdk_cmd.run_raw_cli("package install {} --yes --cli".format(KAFKA_PACKAGE_NAME))
 
-    stop_count = sys.argv[2]
-    print("stop_count: {}".format(stop_count))
+    for dispatcher in dispatchers:
+        run_pipeline(stop_count=20,
+                     spark_app_name=dispatcher)
 
-    run_pipeline(kerberos_flag="false",
-                 jar_uri=jar_uri,
-                 num_consumers=num_consumers,
-                 stop_count=stop_count,
-                 spark_app_name=utils.SPARK_APP_NAME)
 
-    # dispatchers_file = sys.argv[1]
-    # print("dispatchers_file: {}".format(dispatchers_file))
-    # with open(dispatchers_file) as f:
-    #     dispatchers = f.read().splitlines()
-    # print("dispatchers: {}".format(dispatchers))
-    #
     # launch_rate_per_min = int(sys.argv[2])
     # print("launch_rate_per_min: {}".format(launch_rate_per_min))
     #
